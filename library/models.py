281269841587c66a6b18bb45b4dc92c6904b9d67
Michael <sorokajr678@bk.ru>
2025-10-17 13:56:10 +0300

    create models Book, Author

--- library/models.py
+++ library/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='authors/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    description = models.TextField()
+    preview_image = models.ImageField(upload_to='book_covers/')
+
+    content_text = models.TextField(
+        blank=True,
+        default='Текст книги временно недоступен'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.author.name}"
