ae6f70893873fd32333cadc2d918f330a36fc332
Michael <sorokajr678@bk.ru>
2025-10-18 17:12:37 +0300

    create login, add JWT

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('library.urls', namespace='library'))
+    path('', include('library.urls', namespace='library')),
+    path('', include('users.urls', namespace='users')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'users'
 
@@ -9,5 +13,6 @@ router.register(r'users', UserViewSet, basename='users')
 
 
 urlpatterns = [
-
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
