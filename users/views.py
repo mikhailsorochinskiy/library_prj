c85dcfd0a3612c1d3c802d28e972fb09f16e06b1
Michael <sorokajr678@bk.ru>
2025-10-18 16:52:39 +0300

    create User viewset

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['create', 'login']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
