86d483845b357d8203ca768bfec382dad1868606
Michael <sorokajr678@bk.ru>
2025-12-02 16:21:33 +0300

    new models

--- library/models.py
+++ library/models.py
@@ -15,11 +15,26 @@ class Author(models.Model):
 
 
 class Book(models.Model):
+    DISPOSITION_CHOICES = [
+        ("Socializing", "Социализирующая"),
+        ("Cultural-Educational", "Культурно-познавательная"),
+        ("Aesthetic", "Эстетическая"),
+        ("Emotional-Emphatic", "Эмоционально-эмпатическая"),
+        ("Philosophical-Ideological", "Философско-мировоззренческая"),
+        ("Existential", "Экзистенциальная"),
+        ("Optimizing", "Оптимизирующая"),
+        ("Entertaining", "Развлекательная"),
+        ("Escapist", "Эскапическая"),
+        ("Undifferentiated", "Недифференцированная"),
+    ]
+
     title = models.CharField(max_length=200, verbose_name="Название")
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="Автор")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="Author")
     description = models.TextField(verbose_name="Описание", blank=True, null=True)
     preview_image = models.ImageField(upload_to='book_covers/', blank=True, null=True, verbose_name="Превью")
     genre = models.CharField(max_length=50, verbose_name="Жанр", blank=True, null=True)
+    disposition = models.CharField(max_length=50, choices=DISPOSITION_CHOICES, verbose_name="Читательская диспозиция",
+                                   blank=True, null=True)
 
     text_link = models.TextField(
         blank=True,
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from library.models import Book, Author
 
 
 class User(AbstractUser):
@@ -18,3 +19,27 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.email}'
+
+
+class SelectedBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="book")
+
+    class Meta:
+        verbose_name = 'Избранная книга'
+        verbose_name_plural = 'Избранные книги'
+
+    def __str__(self):
+        return f'Книга {self.book} добавлена в избранное у пользователя {self.user}'
+
+
+class SubscribeAuthor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="author")
+
+    class Meta:
+        verbose_name = 'Подписка на автора'
+        verbose_name_plural = 'Подписки на автора'
+
+    def __str__(self):
+        return f'Пользователь {self.user} подписался на автора {self.author}'
