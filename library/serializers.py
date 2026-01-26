122db2aa7fdaa9714fe7b8b21348338bd10bb0f9
Michael <sorokajr678@bk.ru>
2026-01-26 15:39:42 +0300

    change in book serializer

--- library/serializers.py
+++ library/serializers.py
@@ -1,11 +1,20 @@
 from .models import Author, Book
 from rest_framework import serializers
+from users.models import Comment
+
 
 
 class BookSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField()
+
+    def get_comments(self, obj):
+        from users.serializers import CommentSerializer
+        comments = obj.comment_set.all()
+        return CommentSerializer(comments, many=True).data
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'title', 'author', 'description', 'preview_image', 'genre', 'disposition', 'comments')
 
 
 class AuthorSerializer(serializers.ModelSerializer):
