ddeda4ae05507437edbbc89770909eb718a7c48d
Michael <sorokajr678@bk.ru>
2026-02-07 16:20:54 +0300

    cors

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,9 +5,11 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email='admin@mail.ru')
-        user.set_password('admin')
-        user.is_active = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
+        user = User.objects.filter(email='admin@mail.ru')
+        if not user.exists():
+            user = User.objects.filter(email='admin@mail.ru')
+            user.set_password('admin')
+            user.is_active = True
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
