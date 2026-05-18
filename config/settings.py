aeafa2c42d2454e0cc7e10341063311d07b3fabe
Michael <sorokajr678@bk.ru>
2026-05-18 18:57:41 +0300

    email smpt google

--- config/settings.py
+++ config/settings.py
@@ -218,16 +218,16 @@ CORS_ALLOW_HEADERS = [
 ]
 
 # На время разработки письма будут падать в консоль терминала
-# if os.getenv('EMAIL_HOST_USER'):
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#     EMAIL_HOST = 'smtp.yandex.ru'
-#     EMAIL_PORT = 465
-#     EMAIL_USE_TLS = False
-#     EMAIL_USE_SSL = True
-#     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-#     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-#     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-#
-# else:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@library.com'
+if os.getenv('EMAIL_HOST_USER'):
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.yandex.ru'
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'noreply@library.com'
