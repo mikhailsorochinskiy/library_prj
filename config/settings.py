13581e5fa20002d827cdf4bb1e8ed461edfa2cad
Michael <sorokajr678@bk.ru>
2026-05-18 21:35:41 +0300

    email smpt google

--- config/settings.py
+++ config/settings.py
@@ -220,10 +220,10 @@ CORS_ALLOW_HEADERS = [
 # На время разработки письма будут падать в консоль терминала
 if os.getenv('EMAIL_HOST_USER'):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.yandex.ru'
-    EMAIL_PORT = 465
-    EMAIL_USE_TLS = False
-    EMAIL_USE_SSL = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
